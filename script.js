9aaf0c0ef1b716a1d8ef5e871e70a4c56bb6a37a
John Bouchard <jbouchard13@gmail.com>
2020-04-23 18:24:19 -0400

    linked page elements and created a countdown timer

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 // Link the elements that need to be changed on the page
+var titleElement = document.querySelector("#title");
+var timerElement = document.querySelector("#timer");
+var textboxElement = document.querySelector("#textbox");
+var rightWrongElement = document.querySelector("#right-wrong");
+var startButtonElement = document.querySelector("#start");
 
 // Create an event listener for the start button
+startButtonElement.addEventListener("click", function () {
 
-// Create a timer that counts down each second after the start button is clicked
+    // Create a timer that counts down each second after the start button is clicked
+    var secondsLeft = 75;
+
+    var secondsInterval = setInterval(function () {
+        if (secondsLeft > 0) {
+            secondsLeft--;
+            timerElement.textContent = secondsLeft;
+        } else if (secondsLeft === 0) {
+            clearInterval(secondsInterval);
+            alert("Game over")
+        }
+    }, 1000);
+});
 
 // Create an array of objects that contain the questions with multiple choices for answers
 
