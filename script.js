0ed106d4ac21cb914ffc489c120e67964a8716aa
John Bouchard <jbouchard13@gmail.com>
2020-04-23 16:57:42 -0400

    added js file with comment pseudo code

---
+++ script.js
@@ -0,0 +1,23 @@
+// Link the elements that need to be changed on the page
+
+// Create an event listener for the start button
+
+// Create a timer that counts down each second after the start button is clicked
+
+// Create an array of objects that contain the questions with multiple choices for answers
+
+// Display the objects in the textbox
+
+// If the answer is correct, display correct and add to score
+
+// If the answer is incorrect, display incorrect and subtract from timer
+
+// Once timer hits 0 or all questions answered, display the users score and an input for their initials
+
+// Save the user's score and initials to local storage so it persists between page loads
+
+// Display the users score and initials on the highscores page
+
+// Make the clear button clear the local storage and the highscores
+
+// Make the play again button bring the user back to the home page
