7f83857fb23a603605a65547449c1dc2fc74048e
John Bouchard <jbouchard13@gmail.com>
2020-04-25 19:36:49 -0400

    added more pseudo code and restructured some functions

--- script.js
+++ script.js
@@ -1,41 +1,85 @@
-// Link the elements that need to be changed on the page
-var titleElement = document.querySelector("#title");
+// When the user first accesses the page, they are shown an intro message
+// In the html, we could have <div> with the intro. id #intro
+// Users can click the start button, which will start the timer
+
+// ************** LINKING PAGE ELEMENTS *******************
 var timerElement = document.querySelector("#timer");
-var textboxElement = document.querySelector("#textbox");
-var rightWrongElement = document.querySelector("#right-wrong");
-var startButtonElement = document.querySelector("#start");
+var introElement = document.querySelector("#intro");
+var startButtonElement = document.querySelector("#start-btn");
 
-// Create an event listener for the start button
+// Create variable to store currentCount, default to 0
+var currentCount = 0;
+
+// ********************* QUESTIONS ************************
+// Create variable to store the array of questions
+var questions = [
+  {
+    question: "",
+    answers: [],
+    correctAnswer: 2,
+  },
+  {
+    question: "",
+    answers: [],
+    correctAnswer: 2,
+  },
+  {
+    question: "",
+    answers: [],
+    correctAnswer: 2,
+  },
+  {
+    question: "",
+    answers: [],
+    correctAnswer: 2,
+  },
+];
+
+// Create variable to store the index of the current question they are answering
+//  - (string) question
+//  - array of possible answers (list)
+//  - correct answer (number, index of the answer from array of possible answers)
+
+// ********************** QUIZ START *********************
+// When the user clicks on start:
 startButtonElement.addEventListener("click", function () {
-
-    // Create a timer that counts down each second after the start button is clicked
-    var secondsLeft = 75;
-
-    var secondsInterval = setInterval(function () {
-        if (secondsLeft > 0) {
-            secondsLeft--;
-            timerElement.textContent = secondsLeft;
-        } else if (secondsLeft === 0) {
-            clearInterval(secondsInterval);
-            alert("Game over")
-        }
-    }, 1000);
+  // - create an event listener for the start button
+  // - hide the intro <div>
+  // - hide the start button (#start-btn)
+  // - start timer with countdownTimer function
+  countdownTimer();
 });
 
-// Create an array of objects that contain the questions with multiple choices for answers
+// - create a setInterval of 75000ms.
+function countdownTimer() {
+  currentCount = 10;
+  // - update the currentCount to 75
+  var countInterval = setInterval(function () {
+    if (currentCount > 0) {
+      // - every time the interval runs, we need to decrement the current count by 1, update the currentCount
+      currentCount--;
+      timerElement.textContent = currentCount;
+    } else if (currentCount === 0) {
+      // - Once timer hits zero, interval is cleared so it doesn't go below 0
+      clearInterval(countInterval);
+    }
+  }, 1000);
+}
 
-// Display the objects in the textbox
+// - render the first question
+// - using DOM manipulation, create a new element (document.createElement) with the question info, and display it on screen (appendChild)
 
-// If the answer is correct, display correct and add to score
+// Once the game has started, display the first question, with buttons for the answers
 
-// If the answer is incorrect, display incorrect and subtract from timer
+// When a user clicks on one of those questions, determine if it is correct or incorrect, display the next question, and display the status of their answer (correct/incorrect, for a short time)
 
-// Once timer hits 0 or all questions answered, display the users score and an input for their initials
+// ********************** QUIZ END *************************
 
-// Save the user's score and initials to local storage so it persists between page loads
+// End condition
+//  - generate endGame function to use for both end conditions
+//  - User answers all of the questionds OR
+//  - Time runs out
 
-// Display the users score and initials on the highscores page
+// When the game ends, allow the user to enter their initials and store their score
 
-// Make the clear button clear the local storage and the highscores
-
-// Make the play again button bring the user back to the home page
+// Redirect the user to the highscores page, which will display all of the stored high scores
